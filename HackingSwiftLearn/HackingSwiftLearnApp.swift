c7672109bc5b9015f4e43d9250b338ab275dabb0
Mert can Ünek <mrtcnunk@gmail.com>
2024-07-29 15:10:59 +0300

    Initial Commit

---
+++ HackingSwiftLearn/HackingSwiftLearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackingSwiftLearnApp.swift
+//  HackingSwiftLearn
+//
+//  Created by Mertcan Ünek on 29.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct HackingSwiftLearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
