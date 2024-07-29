2d8c77fc2fa4adc05563a9a958e4fe00af7545ea
Mert can Ünek <mrtcnunk@gmail.com>
2024-07-29 17:46:26 +0300

    daily commit
    This SwiftUI component, CompainsListRow, displays a navigable list of menu sections and their respective items. It dynamically loads data from a JSON file, utilizes NavigationView and List for a structured UI, and features reusable components for displaying individual menu items.

---
+++ HackingSwiftLearn/Feature/BuildinMenu/CompainsListRowView.swift
@@ -0,0 +1,61 @@
+//
+//  CompainsListRowView.swift
+//  HackingSwiftLearn
+//
+//  Created by Mertcan Ünek on 29.07.2024.
+//
+
+import SwiftUI
+
+struct CompainsListRow: View {
+    private let menuItem: [MenuSection]
+    
+    init() {
+        menuItem = Bundle.main.decodeList(key: .menu, type: MenuSection.self)
+    }
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(menuItem) { section in
+                    Section {
+                        ForEach(section.items) { item in
+                            ItemRow(item: item)
+                        }
+                    } header: {
+                        Text(section.name)
+                    }
+
+                }
+            }
+            .navigationTitle("Menu")
+            .listStyle(.grouped)
+        }
+    }
+}
+
+struct ItemRow: View {
+    let item: MenuItem
+    
+    var body: some View {
+        HStack {
+            Image(item.thumbnailImage) // Make sure `thumbnailImage` is a valid image name or resource
+            VStack(alignment: .leading) {
+                Text(item.name)
+                Text("\(item.price)$")
+            }
+        }
+    }
+}
+
+struct CompainsListRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        CompainsListRow()
+    }
+}
+
+struct ItemRow_Previews: PreviewProvider {
+    static var previews: some View {
+        CompainsListRow()
+    }
+}
